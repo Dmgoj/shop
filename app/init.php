7ecf7066b022ecebec6a1fba2819011965b4d3af
Domagoj Grgić <domagoj.grgic@outlook.com>
2020-09-28 11:23:16 +0200

    First Commit

---
+++ app/init.php
@@ -0,0 +1,28 @@
+<?php
+/*
+
+    INIT
+    Basic configuration settings
+
+*/
+
+// connect to database
+
+$server='localhost';
+$user='root';
+$pass='';
+$db='shop';
+
+$database=new mysqli($server,$user,$pass,$db);
+
+// error reporting
+
+mysqli_report(MYSQLI_REPORT_ERROR);
+ini_set('display_errors', 1);
+
+// setup constants
+
+define('SITE_NAME', 'Online Store');
+define('SITE_PATH','http://shop/');
+define('IMAGE_PATH','http://shop/resources/images/');
+
---
+++ app/views/includes/public_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="resources/css/style.css" media="all" rel="stylesheet" type="text/css">
+    <title>Online Store</title>
+</head>
+<body class="">
+    <div id="wraper">
+        <div class="secondarynav">
+            <strong>0 items ($0.00) in the cart</strong> &nbsp;| &nbsp;
+            <a href="<?php echo SITE_PATH;?>cart.php">Shopping Cart</a>
+        </div>
+
+        <h1><?php echo SITE_NAME; ?></h1>
+
+        <ul class="nav">
+            <li class="active"><a href="#">All Items</a></li>
+            <li ><a href="#">Category 1</a></li>
+        </ul>
+    
+</body>
+</html>
